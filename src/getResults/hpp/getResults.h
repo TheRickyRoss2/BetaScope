e004c68a2d7fe4088e3043e7d2e789295f7bb43c
neko-0 <yuzhan.physics@gmail.com>
2018-02-28 19:44:06 -0800

    cleanup format

--- src/getResults/hpp/getResults.h
+++ src/getResults/hpp/getResults.h
@@ -15,7 +15,8 @@ class getData
 	int Trigger = 2;
 
 	double tmax_min, tmax_max, pmax_min, pmax_max;
-	int ini_bin = 100;
+
+	int    ini_bin = 100;
 	double ini_rangeMin = 1;
 	double ini_rangeMax = 1;
 
@@ -25,17 +26,10 @@ class getData
 
 	std::vector<std::vector<std::string>> plotArg[2] = {};
 
-
-	//std::string cuts;
-
 	std::string current_file;
-
-	//--------8 11 2017-------//
 
 	int landau_left  = 5;
 	int landau_right = 20;
-
-	//------------------------//
 
 	public:
 		std::string public_cuts = "no cuts yet";
@@ -43,24 +37,27 @@ class getData
 		void setPlotArg();
 
 
-		void max( const char* path,
-		          int         dut,
-		          int         trig,
-		          TCut        cuts     = "",
-		          bool        saveFile = false,
-		          double      Ymin     = -1.0,
-		          double      Ymax     = -1.0,
-		          double      Xmax     = -1.0 );
+		void max(
+			const char* path,
+			int         dut,
+			int         trig,
+			TCut        cuts     = "",
+			bool        saveFile = false,
+			double      Ymin     = -1.0,
+			double      Ymax     = -1.0,
+			double      Xmax     = -1.0
+		);
 
-		void magic( const char* path,
-		            double      tmax2_min,
-		            double      tmax2_max,
-		            double      pmax2_min,
-		            double      pmax2_max,
-		            double      tmax3_min,
-		            double      tmax3_max,
-		            double      pmax3_min,
-		            double      pmax3_max );
+		void magic(
+			const char* path,
+			double      tmax2_min,
+			double      tmax2_max,
+			double      pmax2_min,
+			double      pmax2_max,
+			double      tmax3_min,
+			double      tmax3_max,
+			double      pmax3_min,
+			double      pmax3_max );
 
 		void set_ch (int dut = 2, int trigger = 3);
 
@@ -68,45 +65,60 @@ class getData
 
 		void adjust_hist (bool on_off);
 
-		TCut setcuts (double Dut_tmin,  double Dut_tmax,  double Dut_pmin,  double Dut_pmax,
+		TCut setcuts(
+			double Dut_tmin,
+			double Dut_tmax,
+			double Dut_pmin,
+			double Dut_pmax,
+			double Trig_tmin,
+			double Trig_tmax,
+			double Trig_pmin,
+			double Trig_pmax,
+			int DUT,
+			int Trigger
+		);
 
-		              double Trig_tmin, double Trig_tmax, double Trig_pmin, double Trig_pmax,
+		std::tuple<double,double,int,double,double> Make_Histogram(
+			const char* path,
+			std::string HistName,
+			std::string expression,
+			std::string FitFunc,
+			TCut        cuts,
+			int         Channel,
+			int         binNum,
+			double      range_min,
+			double      range_max,
+			int         draw     = 1,
+			bool        saveFile = false,
+			bool        Roo      = false,
+			bool        confine  = false
+		);
 
-		              int DUT, int Trigger);
+		void pulses(
+			const char* path,
+			TCut        cuts,
+			int         DUT,
+			int         Trigger,
+			int         draw = 1
+		);
 
-		std::tuple<double,double,int,double,double> Make_Histogram( const char* path,
-		                                         std::string HistName,
-		                                         std::string expression,
-		                                         std::string FitFunc,
-		                                         TCut        cuts,
-		                                         int         Channel,
-		                                         int         binNum,
-		                                         double      range_min,
-		                                         double      range_max,
-		                                         int         draw     = 1,
-		                                         bool        saveFile = false,
-		                                         bool        Roo      = false,
-		                                         bool        confine  = false );
-
-		void pulses( const char* path,
-                     TCut        cuts,
-                     int         DUT,
-                     int         Trigger,
-                     int         draw = 1 );
-
-		void processHistogram( const char* path,
-		             double Dut_tmin,  double Dut_tmax,  double Dut_pmin,  double Dut_pmax,
-		             double Trig_tmin, double Trig_tmax, double Trig_pmin, double Trig_pmax,
-		             bool saveFile = false,
-		             bool logging  = true );
-
+		void processHistogram(
+			const char* path,
+			double Dut_tmin,
+			double Dut_tmax,
+			double Dut_pmin,
+			double Dut_pmax,
+			double Trig_tmin,
+			double Trig_tmax,
+			double Trig_pmin,
+			double Trig_pmax,
+			bool saveFile = false,
+			bool logging  = true
+		);
 
 		void see_status ();
 
 		void landau_hist_factor ( int left, int right );
-
-		//void single_hist ( const char* path, std::string hist_type );
-
 };
 
 #endif
